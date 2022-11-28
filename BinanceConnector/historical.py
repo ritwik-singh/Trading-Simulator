75113ae044de19ab221f8f5b372d04e1c4f8d3d3
Ritwik Kumar Singh <31265500+ritwik-singh@users.noreply.github.com>
2022-11-27 19:08:40 -0600

    Add files via upload

--- BinanceConnector/historical.py
+++ BinanceConnector/historical.py
@@ -1,15 +1,17 @@
 import asyncio
+from sqlite3 import paramstyle
 from binance import AsyncClient
 from datetime import datetime
 from dateutil.parser import parse
+import pandas as pd
 import time
 import asyncio
+import sys
 
-
-count = 0
+olchv = []
 
 def date_range(start, end, intv):
-    from datetime import datetime
+
     result = []
     start = parse(start)
     start = start.strftime("%Y%m%d")
@@ -25,35 +27,73 @@ def date_range(start, end, intv):
 
 
 
-async def run():
+async def run(symbol, startDate, endDate, interval):
+    
+    """
 
-    result = date_range("1 Dec, 2017","16 Jan, 2022",10)
-    await asyncio.gather(*[fetch_data(result[i],result[i+1]) for i in range(len(result)-1)])
-        
+        making asynchronus call for downloading historical data by 
+        dividing dates in equal intervals.
 
-def processResponse(kline, start):
-    global count
-    count += len(kline)
-    return
+    """
 
-async def fetch_data(start,end):
+
+    result = date_range(startDate,endDate,100)
+    client = await AsyncClient.create()
+    await asyncio.gather(*[fetch_data(result[i],result[i+1],symbol,client) for i in range(len(result)-1)])
+    await client.close_connection()
+
+def processResponse(klines, start):
+
+    global olchv
+    start = datetime.strptime(start,"%d %b, %y")
+    for kline in klines:
+        olchv.append(kline[:5]+[kline[6]])
+
+async def fetch_data(start,end,symbol,client):
+
     try:
-        client = await AsyncClient.create()
+        
         print(f"trying {start}")
-        klines = await client.get_historical_klines("BTCUSDT", AsyncClient.KLINE_INTERVAL_1MINUTE , start, end)
+        klines = await client.get_historical_klines(symbol, AsyncClient.KLINE_INTERVAL_1HOUR , start, end)
         processResponse(klines,start)
-        await client.close_connection()
+        
     except Exception as e:
         print(e)
 
-     
+
+def main(symbol, startDate, endDate, interval):
+
+    start = time.time()
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(run(symbol, startDate, endDate, interval))
+
+    df_list = pd.DataFrame(olchv,columns=["kline_open" , "open_price" ,"high_price" ,"low_price" ,"close_price" ,"kline_close"])
+
+    #sorting the list according to the open candle stick time.
+    sorted_df  = df_list.sort_values(by = 'kline_open')
+
+    sorted_df.to_csv(f"{symbol}.csv", encoding='utf-8', index=False)
 
 
+    print(time.time() - start)
 
-start = time.time()
-loop = asyncio.get_event_loop()
-loop.run_until_complete(run())
-print(count)
-print(time.time() - start)
+if __name__ == "__main__":
+
+    """
+
+        Arguments: 
+        symbol - Cryptocurrency symbol
+        startDate - starting date of OLHCV data in format "1 Dec, 2017"
+        endDate - ending date of OLHCV data in format "1 Dec, 2017"
+        interval - interval of the kline data like 1m 5m 1hr etc.
+
+    """
+
+    symbol = sys.argv[1]
+    startDate = sys.argv[2]
+    endDate = sys.argv[3]
+    interval = sys.argv[4]
+
+    main(symbol, startDate, endDate, interval)
 
     
